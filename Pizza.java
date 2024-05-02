e2ead17ff0b9d2ac2c37abfa21cc7d26c409d8a6
Prince <162778269+princeofcommits@users.noreply.github.com>
2024-05-02 12:21:44 +0530

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -1,5 +1,12 @@
-public class Pizza { int price; String pizza;String bill=""; int basepizza; int veg=200;int non_veg=285;
-   private int extracheese=99;private int extratoppings=150; private int packaging=25; boolean isfarm_pizza=false; private boolean isextrachesseaded=false;private boolean isextratopingsaded=false;private boolean isetakeaway=false;
+public class Pizza { int price; String pizza;String bill="";
+   int basepizza; int veg=200;int non_veg=285;
+   private int extracheese=99;
+   private int extratoppings=150; 
+   private int packaging=25; 
+   boolean isfarm_pizza=false; 
+   private boolean isextrachesseaded=false;
+   private boolean isextratopingsaded=false;
+   private boolean isetakeaway=false;
     public void bill(){
         System.out.println("Bill details");
         if (isfarm_pizza)
@@ -25,12 +32,18 @@ public class Pizza { int price; String pizza;String bill=""; int basepizza; int 
         @Override
         public void addextratoppings() {}
     }
-    public void addextracheese(){price+=extracheese; isextrachesseaded=true;}public void addextratoppings(){price+=extratoppings; isextratopingsaded=true;}public void takeaway(){price+=packaging;isetakeaway=true;}
+    public void addextracheese(){price+=extracheese; isextrachesseaded=true;}
+    public void addextratoppings(){price+=extratoppings; isextratopingsaded=true;}
+    public void takeaway(){price+=packaging;isetakeaway=true;}
     public void placeorder(String name,String pizza,int quantity){
         System.out.println("Your order no is ");System.out.println( );System.out.println();
     }
     public static void main(String[] args) {
         Pizza mypizzza=new Pizza("nonveg");
-         mypizzza.addextracheese(); mypizzza.takeaway(); mypizzza.bill(); farm_pizza p=new farm_pizza("veg"); p.bill();
+         mypizzza.addextracheese();
+         mypizzza.takeaway(); 
+         mypizzza.bill();
+         farm_pizza p=new farm_pizza("veg");
+         p.bill();
     }
 }
